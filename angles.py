774ffc6aa37d422d70fc7d5a68833dcdaf06cca0
Camila Losada <camilosadaa@hotmail.com>
2021-05-26 16:46:12 +0200

    velocity and acceletarion

--- angles.py
+++ angles.py
@@ -5,75 +5,99 @@ import jointPose as jp
 import psutil
 import matplotlib.pyplot as plt
  
-cap = cv2.VideoCapture(0)
-
+cap = cv2.VideoCapture('arm_flexion.mp4')
+fps_video= 30
 plt.rcParams['animation.html'] = 'jshtml'
 fig = plt.figure()
 ax = fig.add_subplot(111)
 fig.show()
+
+
+
 iteration = 0
-x_it, y_ang = [], []
+x_time, y_ang = [], []
 
 detector = jp.poseDetector()
-joints = ('right_elbow','left_elbow,','right_knee','left_knee')
+joints = ('right_elbow','left_elbow','right_knee','left_knee')
 count = 0
 dir = 0
 pTime = 0
 
 while True:
     success, img = cap.read()
-    img = cv2.resize(img, (1280, 720))
-    # img = cv2.imread("AiTrainer/test.jpg")
-    img = detector.findPose(img, False)
-    lmList = detector.findPosition(img, False)
-    # print(lmList)
-    if len(lmList) != 0:
-        
-        angle = detector.findAngle(img, joints[2])
-        
-        #angle = detector.findAngle(img, 11, 13, 15,False)
-        per = np.interp(angle, (210, 310), (0, 100))
-        bar = np.interp(angle, (220, 310), (650, 100))
-        # print(angle, per)
- 
-        # Check for the dumbbell curls
-        color = (255, 0, 255)
-        if per == 100:
-            color = (0, 255, 0)
-            if dir == 0:
-                count += 0.5
-                dir = 1
-        if per == 0:
-            color = (0, 255, 0)
-            if dir == 1:
-                count += 0.5
-                dir = 0
-        print(count)
- 
-        # Draw Bar
-        # cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)
-        # cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED)
-        # cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4,
-        #             color, 4)
- 
-        # Draw Curl Count
-        # cv2.rectangle(img, (0, 450), (250, 720), (0, 255, 0), cv2.FILLED)
-        # cv2.putText(img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15,
-        #             (255, 0, 0), 25)
-        x_it.append(iteration)
-        y_ang.append(angle)
-        ax.plot(x_it, y_ang, color='b')
-        fig.canvas.draw()
-        ax.set_xlim( right=iteration+2)
-        iteration += 1        
- 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5,
-                (255, 0, 0), 5)
- 
-    cv2.imshow("Image", img)
 
+    if success == True:
+        img = cv2.resize(img, (1280, 720))
+    
+        img = detector.findPose(img, False)
+        lmList = detector.findPosition(img, False)
+        # print(lmList)
+        if len(lmList) != 0:
+            
+            angle = detector.findAngle(img, joints[0])
+            
+            #angle = detector.findAngle(img, 11, 13, 15,False)
+            per = np.interp(angle, (210, 310), (0, 100))
+            bar = np.interp(angle, (220, 310), (650, 100))
+            # print(angle, per)
+    
+            # Check for the dumbbell curls
+            color = (255, 0, 255)
+            if per == 100:
+                color = (0, 255, 0)
+                if dir == 0:
+                    count += 0.5
+                    dir = 1
+            if per == 0:
+                color = (0, 255, 0)
+                if dir == 1:
+                    count += 0.5
+                    dir = 0
+            #print(count)
+    
+            # Draw Bar
+            # cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)
+            # cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED)
+            # cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4,
+            #             color, 4)
+    
+            # Draw Curl Count
+            # cv2.rectangle(img, (0, 450), (250, 720), (0, 255, 0), cv2.FILLED)
+            # cv2.putText(img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15,
+            #             (255, 0, 0), 25)
+            x_time.append(iteration*(1/fps_video))
+            y_ang.append(angle)
+            ax.plot(x_time, y_ang, color='b')
+            fig.canvas.draw()
+            ax.set_xlim( right=iteration*(1/fps_video)+(1/fps_video)*2)
+            iteration += 1        
+    
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5,
+                    (255, 0, 0), 5)
+    
+        cv2.imshow("Image", img)
+
+        if (cv2.waitKey(25) & 0xFF == ord('q')):
+            break
+    else:
+        break
+
+angVel = detector.findAngularVelocity(x_time, y_ang)
+angAcc = detector.findAngularAcceleration(x_time, angVel)
+print(len(x_time),len(y_ang),len(angVel),len(angAcc))
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+fig2.show()
+ax2.plot(x_time[1:], angVel, color='r')
+fig3 = plt.figure()
+ax3 = fig3.add_subplot(111)
+fig3.show()
+ax3.plot(x_time[2:], angAcc, color='g')
+
+while True:
     if (cv2.waitKey(25) & 0xFF == ord('q')):
         break
+print('nn')
--- jointPose.py
+++ jointPose.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 import math
- 
+import numpy as np 
  
 class poseDetector():
  
@@ -48,10 +48,14 @@ class poseDetector():
         x1, y1 = self.lmList[self.jointAngles[ja][0]][1:]
         x2, y2 = self.lmList[self.jointAngles[ja][1]][1:]
         x3, y3 = self.lmList[self.jointAngles[ja][2]][1:]
+
+        v21_y, v21_x = y1 - y2, x1 - x2
+        v23_y, v23_x = y3 - y2, x3 - x2
  
         # Calculate the Angle
-        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
-                             math.atan2(y1 - y2, x1 - x2))
+        #angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+        angle = math.degrees(math.acos( ( (v21_y*v23_y) + (v21_x*v23_x) ) /
+                                        ( math.sqrt((v21_y**2) + (v21_x**2)) * math.sqrt((v23_y**2) + (v23_x**2)) ) ))
         # if angle < 0:
         #     angle += 180
  
@@ -70,9 +74,29 @@ class poseDetector():
             cv2.putText(img, str(int(angle)), (x2 - 48, y2 + 52),
                         cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
         return angle
- 
+
+    def findAngularVelocity(self, x_time, y_angle):
+        
+        self.angularVel = []# don't do it with self, change with each angle
+        dx_time = np.diff(x_time)
+        dy_angle = np.diff(y_angle)
+        self.angularVel = [i / j for i, j in zip(dy_angle, dx_time)]
+        
+        return self.angularVel
+
+    def findAngularAcceleration(self, x_time, y_vel):
+        
+        self.angularAcc = [] # don't do it with self, change with each angle
+        dx_time = np.diff(x_time[1:])
+        #dx_time2 = np.diff(dx_time)
+        dy_vel = np.diff(y_vel)
+        
+        self.angularAcc = [i / j for i, j in zip(dy_vel, dx_time)]
+        
+        return self.angularAcc
+
 def main():
-    cap = cv2.VideoCapture(0)#'v4.MP4'
+    cap = cv2.VideoCapture('walking1.mp4')#
     pTime = 0
     detector = poseDetector()
     while True:
@@ -91,6 +115,7 @@ def main():
             cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
                         (255, 0, 0), 3)
     
+            cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
             cv2.imshow("Image", img)
 
             if (cv2.waitKey(25) & 0xFF == ord('q')):
